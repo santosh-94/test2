eacf22a43c7783ee521b9e730edadce1b633c19a
Santosh Sakore <santosh.sakore@softnautics.com>
2020-10-13 15:23:45 +0530

    modified main file craeted copy

---
+++ helloworld_copy.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Running hello world ...!\n");
+	printf("Running hello world 1...!\n");
+	printf("Running hello world 2 ...!\n");
+	printf ("Running hello word after update trigger....!\n");
+	printf ("Running hello word after update trigger 2....!\n");
+
+	return 0;
+}
